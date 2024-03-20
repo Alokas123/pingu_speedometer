ab3926f47e93329159a71749abd374824c39fb92
Pingu <98775320+Alokas123@users.noreply.github.com>
2024-03-20 21:27:22 +0200

    Update config.lua
    Added translations for new carhud command

--- config.lua
+++ config.lua
@@ -1,5 +1,7 @@
 p = {}
-p.Speedo = 'kmh' -- Accepts 'kmh' or 'mph'
+p.speedo = 'kmh' -- Accepts 'kmh' or 'mph'
 
 -- Translations
 p.fuel = 'Fuel'
+p.turnedoff = 'You turned speedometer off'
+p.turnedon = 'You turned speedometer on'
