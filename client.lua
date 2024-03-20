739360cd88045b0c0d98f651f1595c74975e29bb
Pingu <98775320+Alokas123@users.noreply.github.com>
2024-03-20 21:26:10 +0200

    Update client.lua
    added posibility to turn hud off by command /carhud

--- client.lua
+++ client.lua
@@ -1,3 +1,22 @@
+carhud = true
+
+RegisterCommand("carhud", function(source, args, rawCommand)
+    if carhud then
+        carhud = false
+        lib.notify({
+            title = p.turnedoff,
+            type = 'success'
+        })
+    else
+        carhud = true
+        lib.notify({
+            title = p.turnedon,
+            type = 'success'
+        })
+    end
+end)
+
+
 function round(n)
     return math.floor(n * 10 + 0.5) / 10
 end
@@ -9,17 +28,17 @@ end
 lib.onCache('vehicle', function(vehicle)
     if vehicle then
         CreateThread(function()
-            while cache.vehicle do
+            while carhud and cache.vehicle do
                 Wait(100)
                 if GetIsVehicleEngineRunning(cache.vehicle) then
-                    if p.Speedo == 'kmh' then
+                    if p.speedo == 'kmh' then
                     local speed = pyorisus(GetEntitySpeed(cache.vehicle) * 3.6)
                     local fuel = round(GetVehicleFuelLevel(cache.vehicle), 1)
                     lib.showTextUI(('%s km/h     \n     %s: %s'):format(speed, p.fuel, fuel), {
                         position = "right-center",
                         icon = 'tachometer',
                     })
-                    elseif p.Speedo == 'mph' then
+                    elseif p.speedo == 'mph' then
                         local speed = pyorisus(GetEntitySpeed(cache.vehicle) * 2.23694)
                         local fuel = round(GetVehicleFuelLevel(cache.vehicle), 1)
                         lib.showTextUI(('%s MPH     \n     %s: %s'):format(speed, p.fuel, fuel), {
