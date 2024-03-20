c123ff457abf1f9be746551d0adb581f59e25724
Pingu <98775320+Alokas123@users.noreply.github.com>
2024-03-20 21:57:10 +0200

    Update config.lua
    Added posibility to turn fuel display off

--- config.lua
+++ config.lua
@@ -1,6 +1,7 @@
 p = {}
 p.speedo = 'kmh' -- Accepts 'kmh' or 'mph'
 p.command = 'carhud' -- Command used to turn carhud off
+p.usefuel = true -- include fuel display 
 
 -- Translations
 p.fuel = 'Fuel'
