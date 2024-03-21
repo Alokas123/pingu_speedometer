6f29413d18c7efa0046c1e8458b82450022d173b
thejokke <102411171+JOKKEISOLLA@users.noreply.github.com>
2024-03-21 15:21:11 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -4,6 +4,8 @@ p.command = 'carhud' -- Command used to turn carhud off
 p.usefuel = true -- include fuel display 
 
 -- Translations
-p.fuel = 'Fuel'
-p.turnedoff = 'You turned speedometer off'
-p.turnedon = 'You turned speedometer on'
+p.locale = {
+  fuel = 'Fuel',
+  turnedoff = 'You turned speedometer off',
+  turnedon = 'You turned speedometer on'
+}
