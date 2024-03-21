5e46d5fc9d095ed8d0005fd4619ef48ce72df5a8
Pingu <98775320+Alokas123@users.noreply.github.com>
2024-03-21 20:07:42 +0200

    Update config.lua
    change position using config

--- config.lua
+++ config.lua
@@ -2,6 +2,7 @@ p = {}
 p.speedo = 'kmh' -- Accepts 'kmh' or 'mph'
 p.command = 'carhud' -- Command used to turn carhud off
 p.usefuel = true -- include fuel display 
+p.position = 'bottom-center' -- 'right-center' or 'left-center' or 'top-center' or 'bottom-center'
 
 -- Translations
 p.locale = {
